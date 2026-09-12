17720c82c273449629a2f2d49783990d11ad569f
Airah Shayne T. Sartagoda <atsartagoda@up.edu.ph>
2026-09-12 18:33:09 +0800

    Add dictionary conversion methods

--- src/demo.py
+++ src/demo.py
@@ -6,3 +6,22 @@ print(p.id)
 print(p.lon, p.lat)
 print(p.to_tuple())
 print(p.geometry.geom_type)
+
+print()
+print(p.as_dict())
+
+d = {
+    "id": "B",
+    "lon": 121.05,
+    "lat": 14.65,
+    "name": "Library",
+    "tag": "POI",
+}
+
+q = Point.from_dict(d)
+
+print()
+print(q.id)
+print(q.lon, q.lat)
+print(q.name)
+print(q.is_poi())
